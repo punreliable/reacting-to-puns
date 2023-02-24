4db0f50eb4b7f9ef8f62571153117346241bf3e3
Smith <BSmith9@cg9p9d0gyd.mi.corp.rockfin.com>
2023-02-24 17:20:31 -0500

    Dynamic reusable Post component and CSS module

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,2 +1,13 @@
-const Post = () => {};
+import styles from "../components/Post.module.css";
+
+const names = ["Brian", "Smith"];
+
+const Post = (props) => {
+  return (
+    <div className={styles.post}>
+      <p className={styles.author}>{props.author}</p>
+      <p className={styles.text}>{props.body}</p>
+    </div>
+  );
+};
 export default Post;
---
+++ src/components/UI/GlobalFooter.jsx
@@ -0,0 +1,9 @@
+const GlobalFooter = () => {
+	return (
+		<footer>
+			<p className="read-the-docs">Click on the Vite and React logos to learn more</p>
+		</footer>
+	);
+};
+
+export default GlobalFooter;
