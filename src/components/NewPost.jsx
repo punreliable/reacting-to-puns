aa1594c25938a4ff8b635f14d181084ca4259436
Smith <BSmith9@cg9p9d0gyd.mi.corp.rockfin.com>
2023-02-28 10:18:30 -0500

    Finally got state lifted from NewPost to PostsList

--- src/components/NewPost.jsx
+++ src/components/NewPost.jsx
@@ -1,20 +1,22 @@
-import { useState } from 'react';
 import styles from "./NewPost.module.scss";
 
 const NewPost = (props) => {
-
   return (
     <div className={styles}>
       <h3>New Post</h3>
       <form className={styles.form}>
         <p>
           <label htmlFor="body">Text</label>
-          <textarea id="body" required rows={3} onChange={props.onBodyChange} />
+          <textarea id="body" rows={3} onChange={props.onBodyChange} required />
         </p>
-        <p>{enteredBody}</p>
         <p>
           <label htmlFor="name">Your name</label>
-          <input type="text" id="name" required onChange={props.onAuthorChange}  />
+          <input
+            type="text"
+            id="name"
+            required
+            onChange={props.onAuthorChange}
+          />
         </p>
       </form>
     </div>
--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,11 +1,11 @@
-import { useState } from 'react';
+import { useState } from "react";
 import styles from "./PostsList.module.scss";
 import Post from "../components/Post";
 import NewPost from "../components/NewPost";
 
-const PostsList = (props) => {
-  const [enteredBody, setEnteredBody] = useState('');
-  const [enteredAuthor, setEnteredAuthor] = useState('');
+const PostsList = () => {
+  const [enteredBody, setEnteredBody] = useState("");
+  const [enteredAuthor, setEnteredAuthor] = useState("");
 
   const bodyChangeHandler = (event) => {
     setEnteredBody(event.target.value);
@@ -17,7 +17,10 @@ const PostsList = (props) => {
 
   return (
     <>
-     <NewPost onBodyChange={bodyChangeHandler} onAuthorChange={authorChangeHandler} />
+      <NewPost
+        onBodyChange={bodyChangeHandler}
+        onAuthorChange={authorChangeHandler}
+      />
       <ul className={styles.posts}>
         <Post author={enteredAuthor} body={enteredBody} />
         <Post author="Andrew" body="Top Secret!" />
